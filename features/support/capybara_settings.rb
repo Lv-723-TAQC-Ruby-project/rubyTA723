082e947b3f7c3dc664f13f502d7f493cddd9704a
Coffezka <42944711+Coffezka@users.noreply.github.com>
2023-01-03 17:52:16 +0200

    Change driver

--- features/support/capybara_settings.rb
+++ features/support/capybara_settings.rb
@@ -2,13 +2,13 @@ require 'capybara'
 require 'capybara/cucumber'
 require 'selenium-webdriver'
 
-
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app,:browser => :chrome,timeout: 30)
+Capybara.register_driver :site_prism do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome,timeout: 30)
 end
+
 Capybara.configure do |config|
 
-  config.default_driver = :selenium
+  config.default_driver = :site_prism
 
   config.default_max_wait_time = 10
 end
