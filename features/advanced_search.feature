8f0cb07b44b1ea95c8205e20787571901560f9d8
merykul <mariakulv@gmail.com>
2023-01-04 10:41:31 +0300

    test completed

--- features/advanced_search.feature
+++ features/advanced_search.feature
@@ -1,9 +1,9 @@
 Feature: advanced search
 
   Example: Search by item number
-    When I go to EBAY home page
+    When I open EBAY home page
     And I click 'Advanced' search link
     And I enter item number "2B040H" in the 'Enter keywords or item number' field
-    And I click search button
+    And I click on the search button
     Then I should see "Mask" item in the search results
 
--- features/step_definitions/advanced_search.rb
+++ features/step_definitions/advanced_search.rb
@@ -1,22 +1,23 @@
 #not completed
 
-When('I go to EBAY home page') do
+When('I open EBAY home page') do
   @home_page = HomePage.new
   @home_page.load()
 end
 
 When("I click 'Advanced' search link") do
-
+  @home_page.header.click_advanced_search_link
 end
 
 When("I enter item number {string} in the 'Enter keywords or item number' field") do |item_number|
-
+  @advanced_search_page = AdvancedSearchPage.new
+  @advanced_search_page.enter_in_enter_item_number_field(item_number)
 end
 
-When("I click search button") do
-
+When("I click on the search button") do
+  @advanced_search_page.click_search_button
 end
 
 Then("I should see {string} item in the search results") do |expected_title|
-
+  expect(page).to have_content(expected_title)
 end
