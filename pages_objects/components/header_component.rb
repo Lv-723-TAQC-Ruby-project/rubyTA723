53f5d5e28fe061514aaa5033abb42b0283a0f445
merykul <mariakulv@gmail.com>
2023-01-04 10:41:03 +0300

    added advanced search link element

--- pages_objects/components/header_component.rb
+++ pages_objects/components/header_component.rb
@@ -5,9 +5,14 @@ require_relative '../base_page'
 class HeaderComponent < BasePage
 
   element :search_field_element,:xpath, "//div[@id='gh-ac-box']//input[@type='text']"
+  element :advanced_search_link, :xpath, "//td[@id='gh-as-td']"
 
   def type_to_search_field(text)
     search_field_element.set(text)
+  end
+
+  def click_advanced_search_link
+    advanced_search_link.click
   end
 
 end
