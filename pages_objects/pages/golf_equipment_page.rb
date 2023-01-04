7593cfc755d546b126c3df7f5908af26bb2ec3be
Coffezka <42944711+Coffezka@users.noreply.github.com>
2023-01-04 23:30:07 +0200

    Minor fix

--- pages_objects/pages/golf_equipment_page.rb
+++ pages_objects/pages/golf_equipment_page.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class GolfEquipmentPage < BasePage
+class GolfEquipmentPage < BasePageWithHeader
   element :brand_sort, :xpath, "//div[@id='s0-28_2-9-0-1[0]-0-0-6-5-4[1]-flyout']"
   element :brand, :xpath, "//a[@href='https://www.ebay.com/b/TaylorMade-Golf-Equipment/1513/bn_1991085?mag=1']"
   def sort_brand_click
