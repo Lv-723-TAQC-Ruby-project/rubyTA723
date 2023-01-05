8c602b95bdec103323b31f89741f4ffaa5a23c40
vladkovalyov7 <vladkovalyov67@gmail.com>
2023-01-05 14:35:45 +0200

    chang locator in category page

--- pages_objects/pages/category_community_page.rb
+++ pages_objects/pages/category_community_page.rb
@@ -3,7 +3,7 @@
 class CategoryCommunityPage < BasePageWithHeader
 
   element :key_word_field, "input[name='messageSearchField']"
-  element :search_button, "input[id='submitContext_69a9147d99e071']"
+  element :search_button, "input[name='submitContext'] "
 
   def type_key_word_field(key_word)
     key_word_field.set(key_word)
