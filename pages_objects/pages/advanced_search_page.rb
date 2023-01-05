555a46eef8e2c9ae2c55c8dc8e9833c0dbd81a36
merykul <mariakulv@gmail.com>
2023-01-05 21:19:19 +0300

    trying to modify my test

--- pages_objects/pages/advanced_search_page.rb
+++ pages_objects/pages/advanced_search_page.rb
@@ -1,18 +1,22 @@
 require_relative '../base_page_with_header'
 
 class AdvancedSearchPage < BasePageWithHeader
-
   element :enter_item_number_field, :xpath, "//input[@id='_nkw']"
   element :search_button, :xpath, "//button[contains(text(),'Search')]", match: :first
   element :find_stores_link, :xpath, "//a[contains(text(),'Find Stores')]"
-  element :enter_store_name_field, :xpath, "//input[@name='store_search']"
+  element :enter_store_name_field, :xpath, "//input[@id='store_search']"
+  element :find_items_link, :xpath, "//a[contains(text(),'Find items')]"
 
   def enter_in_enter_store_name_field(store_name)
     enter_store_name_field.set(store_name)
   end
 
-  def click_find_stores_button
+  def click_find_stores_link
     find_stores_link.click
+  end
+
+  def click_find_items_link
+    find_items_link.click
   end
 
   def enter_in_enter_item_number_field(item_number)
@@ -22,5 +26,4 @@ class AdvancedSearchPage < BasePageWithHeader
   def click_search_button
     search_button.click
   end
-
 end
