008fbc9198eded94e661299e772a8c8af14450e5
merykul <mariakulv@gmail.com>
2023-01-12 18:02:45 +0300

    corrected url

--- spec/helpers/ebay_api.rb
+++ spec/helpers/ebay_api.rb
@@ -1,5 +1,5 @@
 class RequestApi
-  URL = "https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0"
+  URL = "https://sandbox.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0"
 
   def find_item_with_free_shipping(app_id, keyword)
     RestClient.get("#{URL}&SECURITY-APPNAME=#{app_id}&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD=true&paginationInput.entriesPerPage=2&keywords=#{keyword}&itemFilter. name=FreeShippingOnly&itemFilter.value=true")
