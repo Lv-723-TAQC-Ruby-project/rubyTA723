49f75203e2c82a58aa5b33cbddab2c848f61755b
merykul <mariakulv@gmail.com>
2023-01-12 18:03:22 +0300

    wrote instance 'check response body'

--- spec/api/advanced_search_spec.rb
+++ spec/api/advanced_search_spec.rb
@@ -1,21 +1,23 @@
 require_relative '../helpers/ebay_api'
+require 'json'
 
 describe 'test advanced search api' do
   context 'when find item with free shipping' do
     before(:all) do
       @body = {}
-      @response = RequestApi.new.find_item_with_free_shipping("your app id", "tolkien")
+      @response = RequestApi.new.find_item_with_free_shipping("id", "tolkien")
     end
 
-    let(:response_body) {JSON(@response.body)}
+    let(:response_body) { JSON(@response.body) }
 
     it 'check response code' do
       expect(@response.code).to eq 200
     end
 
-    # it 'check response body' do
-    #   #TO DO
-    # end
+    it 'check response body' do
+      puts(response_body)
+      expect(response_body["items"][0]["shippingType"]).to include 'Free'
+    end
   end
 end
 
