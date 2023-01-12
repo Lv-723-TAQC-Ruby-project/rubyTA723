27a8cf7eccb7fc32fd1d029dd35d11c403e6e566
simpleUA <a.lipskiy@yahoo.com>
2023-01-12 10:47:26 +0200

    Add search page steps

---
+++ features/step_definitions/search_page_steps.rb
@@ -0,0 +1,11 @@
+When("I click ending soonest in Sort dropdown") do
+  @search_page = SearchPage.new
+  @search_page.click_sort_dropdown
+  @search_page.select_sort_ending_soonest
+  @search_page.check_if_sort_is_correct
+end
+
+
+Then("I should see items sorted by time left")do
+    expect(@search_page.check_if_sort_is_correct).to be(true )
+end
