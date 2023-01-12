0aec8b6f021a50b46e7cb7dd738cc22f478ddf7a
merykul <mariakulv@gmail.com>
2023-01-12 09:53:29 +0300

    initialized rspec directories

---
+++ spec/configs/capybara_settings.rb
@@ -0,0 +1,12 @@
+require 'capybara'
+require 'webdrivers'
+require 'capybara/dsl'
+require 'pry'
+
+include Capybara::DSL
+
+Capybara.register_driver :selenium_chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+
+Capybara.current_driver = :selenium_chrome
