c25c9f65f48ea766542bca43eaa3aa5331eb8169
merykul <mariakulv@gmail.com>
2023-01-12 20:30:21 +0300

    created add item to basket method

--- spec/helpers/ebay_api.rb
+++ spec/helpers/ebay_api.rb
@@ -4,8 +4,13 @@ require_relative '../configs/env'
 
 class RequestApi
   URL = "https://sandbox.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0"
+  BASE_URL = "https://api.sandbox.ebay.com/buy/browse/v1"
 
   def find_item_with_free_shipping(app_id, keyword)
     RestClient.get("#{URL}&SECURITY-APPNAME=#{app_id}&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD=true&paginationInput.entriesPerPage=2&keywords=#{keyword}&itemFilter. name=FreeShippingOnly&itemFilter.value=true")
+  end
+
+  def add_item_in_basket(body)
+    RestClient.post("#{BASE_URL}/shopping_cart/add_item", body, Authorization: "Bearer #{TOKEN}")
   end
 end
