887b18480c34de2a7f006b61ebed361de0e0a007
vladkovalyov7 <vladkovalyov67@gmail.com>
2023-01-13 23:25:32 +0200

    some change

--- pages_objects/pages/category_community_page.rb
+++ pages_objects/pages/category_community_page.rb
@@ -2,16 +2,13 @@
 
 class CategoryCommunityPage < BasePageWithHeader
   element :key_word_field, "input[name='messageSearchField']"
-  element :search_button, "input[name='submitContext'] "
-<<<<<<< HEAD
-  # element :start_conversation_button, "a[class='lia-button lia-button-primary']", match: :first
+  element :search_button, "input[name='submitContext']"
   element :start_conversation_button, xpath:"//html/body/div[2]/center/div[1]/div/div/div/div[1]/div[3]/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div/p/a"
   element :seller_news_popup_button,"//a[text()='Seller News']"
   element :announcements_button, "a[data-sp='m571.l2913']"
-=======
   element :start_conversation_button, "a[class='lia-button lia-button-primary']", match: :first
   element :seller_news_popup_button, "//a[text()='Seller News']"
->>>>>>> 961cf97ce60dcaadc83121438b9ba7bc24408c56
+
 
   def type_key_word_field(key_word)
     key_word_field.set(key_word)
@@ -34,15 +31,10 @@ class CategoryCommunityPage < BasePageWithHeader
   def select_announcements
     click_button('Announcements')
   end
-<<<<<<< HEAD
 
   def click_announcements_button
     sleep(5)
     announcements_button.click
   end
 
-
-
-=======
->>>>>>> 961cf97ce60dcaadc83121438b9ba7bc24408c56
 end
