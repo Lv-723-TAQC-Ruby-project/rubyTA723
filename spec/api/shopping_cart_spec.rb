02bc083009b28bca5f29865e42d81003e83ce547
merykul <mariakulv@gmail.com>
2023-01-13 18:47:15 +0300

    added 'check error message' example

--- spec/api/shopping_cart_spec.rb
+++ spec/api/shopping_cart_spec.rb
@@ -1,16 +1,19 @@
 # frozen_string_literal: true
 
 describe 'test shopping basket' do
-  context 'add item in the basket' do
+  context 'add item in the basket which does not support Pay Pal ' do
     before(:all) do
-      @body = { 'itemId': '110552832245', 'quantity': '2' }
+      @body = { 'itemId': 'v1|110552873120|0', 'quantity': '2' }
       @response = RequestApi.new.add_item_in_basket(@body)
     end
 
     let(:response_body) { JSON(@response.body) }
 
-    it 'check if order was created' do
+    it 'check response code' do
       expect(@response.code).to eq 409
+    end
+    it 'check error message' do
+      expect(response_body['message']).to eql('The item does not meet the cart criteria and cannot be added to the cart.')
     end
   end
 end
