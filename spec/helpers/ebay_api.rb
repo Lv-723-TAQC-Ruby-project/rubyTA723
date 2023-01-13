67f96304cad0b8b403d33c4c1d013764d6a1ae0e
merykul <mariakulv@gmail.com>
2023-01-13 18:48:05 +0300

    adjusted add_item_in_basket method with valid url

--- spec/helpers/ebay_api.rb
+++ spec/helpers/ebay_api.rb
@@ -8,7 +8,7 @@ class RequestApi
   URL = 'https://api.sandbox.ebay.com/buy/browse/v1'
 
   def add_item_in_basket(body)
-    RestClient.post("#{URL}/shopping_cart/add_item&X-EBAY-C-MARKETPLACE-ID:EBAY_US", body,
+    RestClient.post("#{URL}/shopping_cart/add_item", body,
                     Authorization: "Bearer #{TOKEN_APP}", marketplace_id: "EBAY_US")
   end
 
