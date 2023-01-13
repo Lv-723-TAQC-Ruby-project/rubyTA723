c0d0f5635e64103be09e407bb95386ec17283739
merykul <mariakulv@gmail.com>
2023-01-13 17:57:22 +0300

    adjusted headers in add_item_in_basket method

--- spec/helpers/ebay_api.rb
+++ spec/helpers/ebay_api.rb
@@ -5,19 +5,15 @@ require 'json'
 require_relative '../configs/env'
 
 class RequestApi
-  SERVICE_URL = 'https://sandbox.ebay.com/services/search/FindingService/v1'
-  BROWSE_URL = 'https://api.sandbox.ebay.com/buy/browse/v1'
+  URL = 'https://api.sandbox.ebay.com/buy/browse/v1'
 
-  def free_shipping
-    RestClient.get("#{SERVICE_URL}", operation_name: 'findItemsAdvanced', content_type: :json, accept: :json, security_appname: "#{APP_NAME}")
-  end
   def add_item_in_basket(body)
-    RestClient.post("#{BROWSE_URL}/shopping_cart/add_item&X-EBAY-C-MARKETPLACE-ID:EBAY_US", body,
-                    Authorization: "Bearer #{TOKEN_APP}")
+    RestClient.post("#{URL}/shopping_cart/add_item&X-EBAY-C-MARKETPLACE-ID:EBAY_US", body,
+                    Authorization: "Bearer #{TOKEN_APP}", marketplace_id: "EBAY_US")
   end
 
   def get_by_legacy_id(legacy_id)
-    RestClient.get("#{BROWSE_URL}/item/get_item_by_legacy_id?legacy_item_id=#{legacy_id}",
+    RestClient.get("#{URL}/item/get_item_by_legacy_id?legacy_item_id=#{legacy_id}",
                    Authorization: "Bearer #{TOKEN}")
   end
 end
