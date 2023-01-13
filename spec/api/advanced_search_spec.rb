22ae22f40550c3ec1432f5f09c18d374df1dfd95
merykul <mariakulv@gmail.com>
2023-01-13 17:56:19 +0300

    added 'check response body'

--- spec/api/advanced_search_spec.rb
+++ spec/api/advanced_search_spec.rb
@@ -1,19 +1,18 @@
-require_relative '../helpers/ebay_api'
-require 'json'
+# frozen_string_literal: true
 
-describe 'test api' do
+describe 'test Browse Api, item' do
   context 'get item by legacy id' do
     before(:all) do
       @body = {}
       @response = RequestApi.new.get_by_legacy_id(110552832245)
     end
     let(:response_body) { JSON(@response.body) }
-
     it 'check response code' do
       expect(@response.code).to eq 200
     end
-    it 'try' do
-      puts(response_body)
+    it 'check response body' do
+      expect(response_body["categoryPath"]).to eq("Cameras & Photo|Camera Drones")
+      expect(response_body["title"]).to eq("Protocol Kaptur GPS II Wi-Fi Drone with HD Camera")
     end
   end
 end
