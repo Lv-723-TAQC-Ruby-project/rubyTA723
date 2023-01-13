3f75e2fe3961e7e4abe869e7b7399b24df4c18c9
merykul <mariakulv@gmail.com>
2023-01-13 17:57:47 +0300

    changed body values

--- spec/api/shopping_cart_spec.rb
+++ spec/api/shopping_cart_spec.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
-# not completed
-
 describe 'test shopping basket' do
   context 'add item in the basket' do
     before(:all) do
-      @body = { 'itemId': 'v1|110552832245|0', 'quantity': '2' }
+      @body = { 'itemId': '110552832245', 'quantity': '2' }
       @response = RequestApi.new.add_item_in_basket(@body)
     end
 
