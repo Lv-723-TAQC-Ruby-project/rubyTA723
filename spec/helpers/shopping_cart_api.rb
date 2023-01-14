15e28b213fe6608b6021144d974e76310bb12c1f
merykul <mariakulv@gmail.com>
2023-01-14 09:10:44 +0300

    created separate file for requests related to shopping cart

---
+++ spec/helpers/shopping_cart_api.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rest-client'
+require 'json'
+require_relative '../configs/env'
+
+class ShoppingCartApi
+  URL = 'https://api.sandbox.ebay.com/buy/browse/v1'
+
+  def add_item_in_basket(body)
+    RestClient.post("#{URL}/shopping_cart/add_item", body,
+                    Authorization: "Bearer #{TOKEN_APP}", marketplace_id: "EBAY_US")
+  end
+end
