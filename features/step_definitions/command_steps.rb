92b2f6a3af70f810d36a4005bb82edab872cfce3
merykul <mariakulv@gmail.com>
2023-01-14 15:29:47 +0300

    moved steps from command_steps.rb to common_steps.rb

--- features/step_definitions/command_steps.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-Given('I am on the Ebay Advanced search page') do
-  @home_page = HomePage.new
-  @home_page.load
-  @home_page.header.click_advanced_search_link
-  @advanced_search_page = AdvancedSearchPage.new
-end
--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -9,3 +9,10 @@ Given('I am on the eBay login page') do
   @login_page = LoginPage.new
   @login_page.load
 end
+
+Given('I am on the Ebay Advanced search page') do
+  @home_page = HomePage.new
+  @home_page.load
+  @home_page.header.click_advanced_search_link
+  @advanced_search_page = AdvancedSearchPage.new
+end
