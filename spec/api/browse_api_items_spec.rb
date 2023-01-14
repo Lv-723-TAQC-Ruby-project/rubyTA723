743194ec15beacbe9c5cd33553f9a025914d4a25
merykul <mariakulv@gmail.com>
2023-01-14 16:28:21 +0300

    moved all api tests, related to Browse API items

---
+++ spec/api/browse_api_items_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+describe "Ebay browse api" do
+  context 'when find certain group of items by group id', :test do
+    before(:all) do
+
+      @response = ItemApi.new.get_group_of_items(110552751758)
+    end
+
+    let(:response_body) { JSON(@response.body) }
+
+    it 'check response code' do
+      expect(@response.code).to eq 200
+    end
+
+    it 'checks the response body' do
+      expect(response_body["items"][0]["title"]).to include( "2Pcs Snap On Protective Case Cover Glass Screen Protector F Apple Watch 41/45MM")
+    end
+  end
+
+  context 'when API codes' do
+    it 'gets response' do
+      response = ItemsGroupIdsApi.new.get_items(385348059626)
+      expect(response.code).to eq 200
+    end
+
+    it 'checks response on the request' do
+      response = ItemsGroupIdsApi.new.get_items(234743947831)
+      expect(response.code).to eq 200
+    end
+  end
+end
