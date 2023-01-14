11d39b8e48c0bfb6124498e4666b4d70a77d9d6d
Nata Kmitiuk <lovelynata9@gmail.com>
2023-01-14 14:12:20 +0200

    added search Api test

--- spec/api/get_item_spec.rb
+++ spec/api/get_item_spec.rb
@@ -1,9 +1,9 @@
 require_relative '../helpers/item_api'
 require 'json'
-describe "ebay browse api" do
+describe "Ebay browse api" do
   context 'when find certain group of items by group id', :test do
     before(:all) do
-      @body = {}
+
       @response = ItemApi.new.get_group_of_items(110552751758)
     end
     let(:response_body) { JSON(@response.body) }
@@ -12,6 +12,9 @@ describe "ebay browse api" do
       expect(@response.code).to eq 200
     end
 
+    it 'checks the response body' do
+      expect(response_body["items"][0]["title"]).to include( "2Pcs Snap On Protective Case Cover Glass Screen Protector F Apple Watch 41/45MM")
+    end
 
   end
 
--- spec/api/search_api_spec.rb
+++ spec/api/search_api_spec.rb
@@ -15,5 +15,15 @@ describe 'Ebay API' do
     it 'check response body' do
       expect(search_result['itemSummaries'][0]['title']).to include 'LG Optimus Phone FreedomPOP LG-LS720VSLRB F3 LTE 1.2 GHz 1.24 GB mem Freedompop'
     end
+
+    before do
+      @response = SearchEbayApi.new.search_refinements('macbook')
+    end
+    it 'checks response code of refinements of items' do
+      expect(@response.code).to eq 200
+    end
+    it 'checks response body' do
+      expect(search_result["refinement"]).to include("categoryDistributions" => [{"categoryId" => "171485", "categoryName" => "Tablets & eBook Readers","refinementHref" => "https://api.sandbox.ebay.com/buy/browse/v1/item_summary/search?q=macbook&limit=3&fieldgroups=ASPECT_REFINEMENTS%2CCATEGORY_REFINEMENTS%2CCONDITION_REFINEMENTS%2CBUYING_OPTION_REFINEMENTS&category_ids=171485"}])
+    end
   end
 end
