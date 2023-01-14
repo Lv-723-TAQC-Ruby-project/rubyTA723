abec7767e7123b1b96a127b1ecc9f8e5da2e0d13
merykul <mariakulv@gmail.com>
2023-01-14 09:11:07 +0300

    renamed

--- spec/helpers/advanced_search_api.rb
+++ spec/helpers/advanced_search_api.rb
@@ -7,11 +7,6 @@ require_relative '../configs/env'
 class RequestApi
   URL = 'https://api.sandbox.ebay.com/buy/browse/v1'
 
-  def add_item_in_basket(body)
-    RestClient.post("#{URL}/shopping_cart/add_item", body,
-                    Authorization: "Bearer #{TOKEN_APP}", marketplace_id: "EBAY_US")
-  end
-
   def get_by_legacy_id(legacy_id)
     RestClient.get("#{URL}/item/get_item_by_legacy_id?legacy_item_id=#{legacy_id}",
                    Authorization: "Bearer #{TOKEN}")
