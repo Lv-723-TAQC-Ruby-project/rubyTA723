faa00fb9e8be3c26e907e5731ecc2afecb585b8b
Volodymyr Zubchynskyi <v.zubchynskyi@teladoc.com>
2023-01-16 13:19:05 +0200

    fix relations

--- spec/helpers/api/metadata_items_condition_policies_api.rb
+++ spec/helpers/api/metadata_items_condition_policies_api.rb
@@ -2,7 +2,7 @@
 
 require 'rest-client'
 require 'json'
-require_relative '../configs/gs/env'
+require_relative '../../configs/env'
 
 class MetadataItemsConditionPoliciesApi
   URL = 'https://api.sandbox.ebay.com/sell/metadata/v1/'
