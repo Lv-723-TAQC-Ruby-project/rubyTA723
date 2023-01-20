3425581e0eb2ca91b91e1f37ac0a343d57bdbe6f
sofiy <sofiya.ometiukh@gmail.com>
2023-01-20 12:45:06 +0200

    fixed marketing API test

--- spec/api/marketing_api_spec.rb
+++ spec/api/marketing_api_spec.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 describe 'Marketing API' do
+  before(:all) do
+    response = Auth.new.log_in(CLIENT_ID, CLIENT_SECRET, SCOPE_API)
+    @token = JSON(response.body)['access_token']
+  end
+
   context 'when search merchandised product' do
     before(:all) do
-      @response = MarketingApi.new.get_merchandised_products
+      @response = MarketingApi.new(@token).get_merchandised_products
     end
 
     let(:search_result) { JSON(@response.body) }
--- spec/helpers/marketing_api.rb
+++ spec/helpers/marketing_api.rb
@@ -3,13 +3,14 @@
 require 'rest-client'
 require 'json'
 require_relative '../configs/env'
+require_relative 'base_api'
 
-class MarketingApi
+class MarketingApi < BaseAPI
   URL = 'https://api.sandbox.ebay.com/buy/marketing/v1_beta'
 
   def get_merchandised_products
     RestClient.get("#{URL}/merchandised_product?category_id=9355&metric_name=BEST_SELLING",
-                   { Authorization: "Bearer #{TOKEN}" })
+                   { Authorization: "Bearer #{@token}" })
   end
 
 end
